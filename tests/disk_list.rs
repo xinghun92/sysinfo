d08d8b4ca40b66b940f2b7fe5d6c4555be548cc2
wangsijie <wangsijie@bytedance.com>
2018-08-03 16:15:01 +0800

    feat: test
    Change-Id: Iae4ce26d234f505a9099aea82ca7b19e9d6d1d83

--- tests/disk_list.rs
+++ tests/disk_list.rs
@@ -14,3 +14,37 @@ fn test_disks() {
     println!("total memory: {}", s.get_total_memory());
     println!("total cpu cores: {}", s.get_processor_list().len());
 }
+
+fn fib(n: u64) -> u64 {
+    if n > 2 {
+        fib(n - 1) + fib(n - 2)
+    } else {
+        1
+    }
+}
+
+#[test]
+fn test_process() {
+    use sysinfo::SystemExt;
+    use sysinfo::ProcessExt;
+    let mut system = sysinfo::System::new();
+    system.refresh_all();
+
+    let pid = sysinfo::get_current_pid();
+
+    let mut i = 30;
+    loop {
+        system.refresh_process(pid);
+        let process = system.get_process(pid).unwrap();
+
+        i += 1;
+        if i > 40 {
+            i = 30;
+        }
+        fib(i);
+        println!("CPU: {}%", process.cpu_usage());
+        println!("Memory: {}", process.memory());
+
+        ::std::thread::sleep_ms(1000);
+    }
+}
